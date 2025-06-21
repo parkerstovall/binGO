ca4fd65b526e1de1955b182b8fa345a2583be2de
parkerstovall <parker.stovall@gmail.com>
2025-06-20 23:07:42 -0500

    done! (#7)
    * got server and client connected, next is UI
    
    * Done!
    
    * Dine!
    
    * stupid merge conflicts

--- binGO.client/client.go
+++ binGO.client/client.go
@@ -3,40 +3,139 @@ package binGO_client
 import (
 	"context"
 	"fmt"
-	"time"
+	"math/rand"
+	"slices"
+	"strconv"
 
 	"github.com/coder/websocket"
+	"github.com/fatih/color"
 )
 
-// This function creates a web socket
-// connection to the server and handles incoming messages.
-func StartClient() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+type binGOclient struct {
+	Board       [][]int
+	CalledBalls []int
+	ctx         context.Context
+}
 
-	conn, _, err := websocket.Dial(ctx, "ws://localhost:8080", &websocket.DialOptions{
-		Subprotocols: []string{"binGO"},
-	})
-
-	if err != nil {
-		fmt.Printf("Failed to connect to server: %s\n", err.Error())
-		return
+func NewClient(ctx context.Context) *binGOclient {
+	client := &binGOclient{
+		Board:       make([][]int, 5),
+		CalledBalls: make([]int, 0),
+		ctx:         ctx,
 	}
 
-	defer conn.Close(websocket.StatusInternalError, "closing")
+	client.initializeBoard()
 
+	return client
+}
+
+func (c *binGOclient) PrintBoard() {
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+
+	uncalled := color.New(color.FgRed).PrintFunc()
+	called := color.New(color.FgGreen).PrintFunc()
+	// Print the Bingo board in a formatted way
+	for i := range c.Board {
+		for j := range c.Board[i] {
+			letters := []string{"B", "I", "N", "G", "O"}
+			msg := fmt.Sprintf("%s%d", letters[j], c.Board[i][j])
+			if c.Board[i][j] < 10 {
+				msg += " "
+			}
+
+			if slices.Contains(c.CalledBalls, c.Board[i][j]) {
+				called(msg + " ")
+			} else {
+				uncalled(msg + " ")
+			}
+		}
+
+		fmt.Println()
+	}
+}
+
+func (c *binGOclient) initializeBoard() {
+	// Initialize the Bingo board
+	for i := range c.Board {
+		c.Board[i] = make([]int, 5)
+
+		// Fill the board with random numbers
+		for j := range c.Board[i] {
+			rand_number := rand.Intn(15) + 1 + (j * 15)
+			c.Board[i][j] = rand_number
+		}
+	}
+}
+
+func (c *binGOclient) CheckForBingo() int {
+	for i := range 5 {
+		// Horizontal
+		if slices.Contains(c.CalledBalls, c.Board[i][0]) &&
+			slices.Contains(c.CalledBalls, c.Board[i][1]) &&
+			slices.Contains(c.CalledBalls, c.Board[i][2]) &&
+			slices.Contains(c.CalledBalls, c.Board[i][3]) &&
+			slices.Contains(c.CalledBalls, c.Board[i][4]) {
+			return 1
+		}
+
+		// Vertical
+		if slices.Contains(c.CalledBalls, c.Board[0][i]) &&
+			slices.Contains(c.CalledBalls, c.Board[1][i]) &&
+			slices.Contains(c.CalledBalls, c.Board[2][i]) &&
+			slices.Contains(c.CalledBalls, c.Board[3][i]) &&
+			slices.Contains(c.CalledBalls, c.Board[4][i]) {
+			return 1
+		}
+	}
+
+	// Diagonal (top-left to bottom-right)
+	if slices.Contains(c.CalledBalls, c.Board[0][0]) &&
+		slices.Contains(c.CalledBalls, c.Board[1][1]) &&
+		slices.Contains(c.CalledBalls, c.Board[2][2]) &&
+		slices.Contains(c.CalledBalls, c.Board[3][3]) &&
+		slices.Contains(c.CalledBalls, c.Board[4][4]) {
+		return 1
+	}
+
+	// Diagonal (top-right to bottom-left)
+	if slices.Contains(c.CalledBalls, c.Board[0][4]) &&
+		slices.Contains(c.CalledBalls, c.Board[1][3]) &&
+		slices.Contains(c.CalledBalls, c.Board[2][2]) &&
+		slices.Contains(c.CalledBalls, c.Board[3][1]) &&
+		slices.Contains(c.CalledBalls, c.Board[4][0]) {
+		return 1
+	}
+
+	return 0
+}
+
+func (c *binGOclient) ListenForCalledBalls(conn *websocket.Conn) {
 	// Handle incoming messages from the server
 	for {
-		readCtx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-		_, msg, err := conn.Read(readCtx)
-		cancel()
+		_, msg, err := conn.Read(c.ctx)
 
 		if err != nil {
-			fmt.Printf("Failed to read message: %s\n", err.Error())
+			err_msg := fmt.Sprintf("Failed to read message: %s\n", err.Error())
+			color.Red(err_msg)
 			continue
 		}
 
-		// Process the received message (e.g., display it)
-		fmt.Println("Received message:", string(msg))
+		ball, err := strconv.Atoi(string(msg))
+		if err != nil {
+			color.Red("Received invalid message from server: %s", string(msg))
+			continue
+		}
+
+		c.CalledBalls = append(c.CalledBalls, ball)
+		c.PrintBoard()
+
+		if c.CheckForBingo() == 1 {
+			color.Green("BINGO! You won!")
+			conn.Write(c.ctx, websocket.MessageText, []byte("BINGO!"))
+		} else {
+			color.Red("No Bingo yet, keep playing!")
+		}
 	}
 }
